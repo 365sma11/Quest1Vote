d7db04e8b550ef922d7a6590ea7c0620466d9fae
sma11 <36231823+365sma11@users.noreply.github.com>
2023-12-14 11:41:53 -0600

    old version 1

--- WWimageview.py
+++ WWimageview.py
@@ -36,19 +36,19 @@ if option:
                     image = Image.open(file_path)
                     current_col.image(image, caption=f"{row['Team']} - Entry: {row['Entry']}", use_column_width=True)
                 elif file_path.endswith('.mp4'):
-                    # current_col.video(file_path)
-                    video_file = open(file_path, 'rb')
-                    video_bytes = video_file.read()
-                    b64_video = base64.b64encode(video_bytes).decode()
+                    current_col.video(file_path)
+                    # video_file = open(file_path, 'rb')
+                    # video_bytes = video_file.read()
+                    # b64_video = base64.b64encode(video_bytes).decode()
 
-                    # Embed the video using HTML with appropriate CSS
-                    current_col.markdown(f"""
-                        <div style="padding: 10px; text-align: center;">
-                            <video style="max-width: 100%; height: auto;" controls>
-                                <source src="data:video/mp4;base64,{b64_video}" type="video/mp4">
-                            </video>
-                        </div>
-                        """, unsafe_allow_html=True)
+                    # # Embed the video using HTML with appropriate CSS
+                    # current_col.markdown(f"""
+                    #     <div style="padding: 10px; text-align: center;">
+                    #         <video style="max-width: 100%; height: auto;" controls>
+                    #             <source src="data:video/mp4;base64,{b64_video}" type="video/mp4">
+                    #         </video>
+                    #     </div>
+                    #     """, unsafe_allow_html=True)
 
                 # Center the link and add a horizontal line
                 current_col.markdown(f"<center><a href='{row['Link']}'>X Link</a></center>", unsafe_allow_html=True)
